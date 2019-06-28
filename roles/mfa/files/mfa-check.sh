3b2f43cc3f0292daeb7803508371586aa9579c0c
Ryan Kraus <rmkraus@gmail.com>
2019-06-28 00:47:43 -0400

    Added some logic to the mfa key generator script.

--- roles/mfa/files/mfa-check.sh
+++ roles/mfa/files/mfa-check.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-if [ ! -e ~/.google_authenticator ]; then
+MAKE_2FA=1
+
+if [[ $UID -eq 0 ]]; then
+        MAKE_2FA=0
+fi
+
+if [[ $(echo $USER | grep -c '^service-') -eq 1 ]]; then
+        MAKE_2FA=0
+fi
+
+if [ -e ~/.google_authenticator ]; then
+        MAKE_2FA=0
+fi
+
+if [[ $MAKE_2FA -eq 1 ]]; then
         /usr/bin/google-authenticator -t -f -l pluto.lab.rmkra.us -d -w 3 -r 3 -R 30
-        /usr/bin/killall -u $USER
+        exit
 fi
